25259af36fee054e73f9ec1f6e74e2259b9d6212
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2024-12-04 08:44:42 +0000

    Completed day 4

---
+++ day4.ts
@@ -0,0 +1,43 @@
+import { countOccurences, readInput } from './utils';
+
+function getElement(grid: string[][], row: number, col: number) {
+	if (row < 0 || row >= lines.length) return '';
+	if (col < 0 || col >= lines[row].length) return '';
+	return grid[row][col];
+}
+
+function getAdjacentStrings(grid: string[][], row: number, col: number, length: number) {
+	const results = new Array<string>(8).fill(getElement(grid, row, col));
+	for (let i = 1; i < length; i++) {
+		results[0] += getElement(grid, row, col + i);
+		results[1] += getElement(grid, row + i, col + i);
+		results[2] += getElement(grid, row + i, col);
+		results[3] += getElement(grid, row + i, col - i);
+		results[4] += getElement(grid, row, col - i);
+		results[5] += getElement(grid, row - i, col - i);
+		results[6] += getElement(grid, row - i, col);
+		results[7] += getElement(grid, row - i, col + i);
+	}
+	return results;
+}
+
+function part1(grid: string[][]) {
+	return grid.reduce((acc1, row, i) => {
+		return acc1 + row.reduce((acc2, _, j) => {
+			const adj = getAdjacentStrings(grid, i, j, 4);
+			return acc2 + countOccurences(adj, 'XMAS');
+		}, 0);
+	}, 0);
+}
+
+function part2(grid: string[][]) {
+	return grid.reduce((acc, row, i) => {
+		return acc + row.filter((_, j) => {
+			const adj = getAdjacentStrings(grid, i, j, 2).filter((_, i) => i % 2 === 1);
+			return adj[0] !== adj[2] && countOccurences(adj, 'AM') == 2 && countOccurences(adj, 'AS') == 2;
+		}).length;
+	}, 0);
+}
+
+const lines = readInput(4).map(line => line.split(''));
+console.log(`Part 1: ${part1(lines)}\nPart 2: ${part2(lines)}`);
--- utils.ts
+++ utils.ts
@@ -11,3 +11,8 @@ export function countOccurences<T>(array: T[], target: T) {
 export function removeElements<T>(array: T[], index: number, amount = 1) {
 	return array.slice(0, index).concat(array.slice(index + amount));
 }
+
+export function compareArray<T>(arr1: T[], arr2: T[]) {
+	if (arr1.length !== arr2.length) return false;
+	return !arr1.some((_, i) => arr1[i] !== arr2[i]);
+}
